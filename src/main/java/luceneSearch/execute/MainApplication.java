2ef1ea281b47de3813b00cdaae65b437329af3b4
aibees <aibees1129@naver.com>
2020-06-17 22:40:52 +0900

    repo init

---
+++ src/main/java/luceneSearch/execute/MainApplication.java
@@ -0,0 +1,10 @@
+package luceneSearch.execute;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.aibees.search.luceneSearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
